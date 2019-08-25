43af6a4fb8e709b22dbc09c4b1331e921c877df2
Gonzalo Mateos <gonzalomateosglezsicilia@gmail.com>
2019-08-25 23:34:41 +0200

    Rest server - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -8,14 +8,14 @@ process.env.PORT = process.env.PORT || 3000;
 //=====================
 // Entorno
 //=====================
-process.env.NOVE_ENV = process.env.NOVE_ENV === 'dev';
+process.env.NOVE_ENV = process.env.NOVE_ENV || 'dev';
 
 //=====================
 // BBDD
 //=====================
 let urlDB;
 
-if (process.env.NOVE_ENV !== 'production') {
+if (process.env.NOVE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
     urlDB = 'mongodb+srv://admin:131184ma@cluster0-rg884.mongodb.net/cafe';
