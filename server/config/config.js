b92edef6e427840c59c3d27a234c4e58567dd8c4
Gonzalo Mateos <gonzalomateosglezsicilia@gmail.com>
2019-08-25 23:39:35 +0200

    Rest server - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -8,7 +8,7 @@ process.env.PORT = process.env.PORT || 3000;
 //=====================
 // Entorno
 //=====================
-process.env.NOVE_ENV = process.env.NOVE_ENV || 'dev';
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 //=====================
 // BBDD
