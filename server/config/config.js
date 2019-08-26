0e7b9f5578d855080e6aae5a3eb74802f676e98d
Gonzalo Mateos <gonzalomateosglezsicilia@gmail.com>
2019-08-26 16:51:57 +0200

    variables de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ console.log(`Cadena de conexion: ${process.env.NOVE_ENV}`);
 if (process.env.NOVE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://admin:131184ma@cluster0-rg884.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 //Metemos la url de la BBDD en el objeto process.
